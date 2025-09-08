bf68b9a0ada05c8df06ed86fb2e438ac80be29c0
Yana <suvorova_ya@outlook.com>
2025-09-08 12:24:58 +0300

    added relationship and ForeignKey for models

--- app/models/categories.py
+++ app/models/categories.py
@@ -1,5 +1,7 @@
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from typing import List, Optional
+from app.models.products import Product
+from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database import Base
 
@@ -9,7 +11,17 @@ class Category(Base):
     __tablename__ = "categories"
     id : Mapped[int] = mapped_column(primary_key=True)
     name : Mapped[str] = mapped_column(String(50),nullable=False)
+    parent_id: Mapped[int | None] = mapped_column(ForeignKey("categories.id"), nullable=True)
     is_active : Mapped[bool] = mapped_column(Boolean, default=True)
+
+    products: Mapped[List["Product"]] = relationship(back_populates="category")
+    parent: Mapped[Optional["Category"]] = relationship(back_populates="children",remote_side="Category.id")
+    children: Mapped[List["Category"]] = relationship(back_populates="parent")
+
+
+
+
+
 
 
 
--- app/models/products.py
+++ app/models/products.py
@@ -2,7 +2,8 @@ from datetime import datetime
 from typing import Optional
 
 from sqlalchemy import String, Float, Integer, Boolean, DateTime
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey
 
 from app.database import Base
 
@@ -12,21 +13,16 @@ class Product(Base):
     __tablename__ = 'products'
     id : Mapped[int] = mapped_column(primary_key=True)
     name : Mapped[str] = mapped_column(String(100),nullable=False)
-    description: Optional[Mapped[str|None]] = mapped_column(String(500))
+    description:Mapped[Optional[str]] = mapped_column(String(500))
     price: Mapped[float] = mapped_column(Float,nullable=False)
-    image_url: Mapped[Optional[str|None]] = mapped_column(String(200))
+    image_url: Mapped[Optional[str]] = mapped_column(String(200))
     stock: Mapped[int] = mapped_column(Integer, nullable=False)
     is_active : Mapped[bool] = mapped_column(Boolean,default=True)
+    category_id: Mapped[int] = mapped_column(ForeignKey("categories.id"), nullable=False)
+    category: Mapped["Category"] = relationship(back_populates="products")
 
 
 
-class Movie(Base):
-    __tablename__ = "movies"
-
-    id : Mapped[int] = mapped_column(primary_key=True)
-    title : Mapped[str] = mapped_column(String(200), nullable=False)
-    duration: Mapped[int] = mapped_column(Integer, nullable=False)
-    release_date : Mapped[Optional[datetime]] = mapped_column(DateTime,nullable=True)
 
 
 
